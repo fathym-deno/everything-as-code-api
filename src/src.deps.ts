fdd01ffff3b631db4e55a7f157dd156281ef01e5
mcgear <michael.gearhardt@gmail.com>
2024-03-20 17:34:11 -0600

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,7 +1,7 @@
-export * from "https://deno.land/std@0.216.0/async/delay.ts";
-export * from "https://deno.land/std@0.216.0/encoding/base64.ts";
-export * from "https://deno.land/x/fathym_common@v0.0.160/mod.ts";
-export * from "https://deno.land/x/fathym_everything_as_code@v0.0.375/mod.ts";
+export * from "https://deno.land/std@0.220.1/async/delay.ts";
+export * from "https://deno.land/std@0.220.1/encoding/base64.ts";
+export * from "https://deno.land/x/fathym_common@v0.0.162/mod.ts";
+export * from "https://deno.land/x/fathym_everything_as_code@v0.0.391/mod.ts";
 
 import Hndlbrs from "npm:handlebars/dist/handlebars.min.js";
 export const Handlebars = Hndlbrs;
