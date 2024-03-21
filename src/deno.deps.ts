cce27f39858fe7c31bf00baff94583fe3b431e91
mcgear <michael.gearhardt@gmail.com>
2024-03-21 14:37:16 -0600

    ref restructure

--- src/deno.deps.ts
+++ src/deno.deps.ts
@@ -1,1 +1,4 @@
 export * from "https://deno.land/x/fathym_everything_as_code@v0.0.395/deno.ts";
+
+import Hndlbrs from "npm:handlebars/dist/handlebars.min.js";
+export const Handlebars = Hndlbrs;
--- src/src.deps.ts
+++ src/src.deps.ts
@@ -2,9 +2,3 @@ export * from "https://deno.land/std@0.220.1/async/delay.ts";
 export * from "https://deno.land/std@0.220.1/encoding/base64.ts";
 export * from "https://deno.land/x/fathym_common@v0.0.175/mod.ts";
 export * from "https://deno.land/x/fathym_everything_as_code@v0.0.395/mod.ts";
-
-import Hndlbrs from "npm:handlebars/dist/handlebars.min.js";
-export const Handlebars = Hndlbrs;
-
-import Sodium from "https://deno.land/x/sodium@0.2.0/basic.ts";
-export const sodium = Sodium;
