75a3c63e158fe2c5da2e999e24f73a86c5edb2d8
mcgear <michael.gearhardt@gmail.com>
2024-08-11 14:09:31 -0600

    missing ref update

--- src/deno.deps.ts
+++ src/deno.deps.ts
@@ -1,4 +1,4 @@
-export * from "jsr:@fathym/eac@0.0.431/deno";
+export * from "jsr:@fathym/eac@0.0.433/deno";
 
 import Hndlbrs from "npm:handlebars@4.7.8/dist/handlebars.min.js";
 export const Handlebars = Hndlbrs;
