69a029d3d401f00aa38d15747de8dd29257176b5
mcgear <michael.gearhardt@gmail.com>
2024-08-20 09:40:18 -0600

    ref updates

--- src/api/.deps.ts
+++ src/api/.deps.ts
@@ -1,11 +1,8 @@
-export {} from "../api/.exports.ts";
+export { type DenoKVNonce } from "jsr:@fathym/common@0/deno-kv";
 
-export {} from "jsr:@fathym/common@0";
-
+// export type { EverythingAsCode } from '../../../everything-as-code/src/eac/.exports.ts';
 export type { EverythingAsCode } from "jsr:@fathym/eac@0";
 export type { EverythingAsCodeClouds } from "jsr:@fathym/eac@0/clouds";
 export type { EverythingAsCodeGitHub } from "jsr:@fathym/eac@0/github";
 export type { EverythingAsCodeIoT } from "jsr:@fathym/eac@0/iot";
 export type { EverythingAsCodeSources } from "jsr:@fathym/eac@0/sources";
-
-export { type DenoKVNonce } from "jsr:@fathym/common@0.2.16/deno-kv";
